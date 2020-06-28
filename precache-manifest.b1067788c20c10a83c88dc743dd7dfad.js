c84b165529478349c5f12f3ecb31f495948e6d30
MICHAEL V.S <michaelvshaji051@gmail.com>
2020-06-28 14:28:20 +0530

    Updates

---
+++ precache-manifest.b1067788c20c10a83c88dc743dd7dfad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b46b2bd512decc5e37af4701656ff88c",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "f1cd4d46edcadbba61e4",
+    "url": "/my-app/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "75072723ebb8c0c10937",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f1cd4d46edcadbba61e4",
+    "url": "/my-app/static/js/2.90494ab2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.90494ab2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75072723ebb8c0c10937",
+    "url": "/my-app/static/js/main.70bb9863.chunk.js"
+  },
+  {
+    "revision": "45ae17e9115fb27d2edf",
+    "url": "/my-app/static/js/runtime-main.8d23f2eb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
